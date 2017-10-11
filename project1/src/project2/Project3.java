d583b920a306cd13930649fdaad40528f84b4963
chandrasekhar009 <31537403+chandrasekhar009@users.noreply.github.com>
2017-10-11 14:15:36 +0530

    commit

---
+++ project1/src/project2/Project3.java
@@ -0,0 +1,19 @@
+package project2;
+
+import java.util.Scanner;
+
+public class Project3 {
+
+	public static void main(String[] args)
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter username");
+		String u=sc.nextLine();
+		System.out.println("enter password");
+		String p=sc.nextLine();
+		//commit
+		//by pull
+	}
+
+}
